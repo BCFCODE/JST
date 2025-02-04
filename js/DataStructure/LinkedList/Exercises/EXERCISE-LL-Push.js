3ff53087b990ba668c9d68e8467b589d0c1d45c3
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-02-04 22:05:39 +0330

    EXERCISE-LL-Push

--- js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.js
+++ js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.js
@@ -47,19 +47,45 @@ class LinkedList {
         this.length = 0;
     }
 
-    push(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
+    /// WRITE PUSH METHOD HERE ///
+    //                          //
+    //                          //
+    //                          //
+    //                          //
+    //////////////////////////////
 
 }
+
+
+function test() {
+    let myLinkedList = new LinkedList(1);
+    myLinkedList.makeEmpty();
+    myLinkedList.push(1);
+    myLinkedList.push(2);
+
+
+    myLinkedList.getHead();
+    myLinkedList.getTail();
+    myLinkedList.getLength();
+    console.log("\nLinked List:");
+    myLinkedList.printList();
+}
+
+
+test();
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2
+
+    Linked List:
+    1
+    2
+
+*/
 
 export default LinkedList
