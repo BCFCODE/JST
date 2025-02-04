5ae38832cc7042eee7696d0422763f60c481955c
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-02-04 22:07:09 +0330

    SOLUTION-LL-Push

--- js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.js
+++ js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.js
@@ -47,12 +47,18 @@ class LinkedList {
         this.length = 0;
     }
 
-    /// WRITE PUSH METHOD HERE ///
-    //                          //
-    //                          //
-    //                          //
-    //                          //
-    //////////////////////////////
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 
 }
 
