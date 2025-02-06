9b0f1fca91e133642f56c10a1c649b4e83131a1a
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-02-06 15:51:29 +0330

    EXERCISE-LL-Constructor

--- js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Constructor.js
+++ js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Constructor.js
@@ -1,17 +1,17 @@
-class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
-}
- 
+// WRITE NODE CLASS HERE //
+//                       //
+//                       //
+//                       //
+//                       //
+///////////////////////////
+
 class LinkedList {
-    constructor(value) {
-        const newNode = new Node(value);
-        this.head = newNode;
-        this.tail = this.head;
-        this.length = 1;
-    }
+	// WRITE LL CONSTRUCTOR HERE //
+	//                           //
+	//                           //
+	//                           //
+	//                           //
+	///////////////////////////////
 
     printList() {
         let temp = this.head;
