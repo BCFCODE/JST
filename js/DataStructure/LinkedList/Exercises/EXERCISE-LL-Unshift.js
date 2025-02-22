34dce5553e5f322ac79822c1e09fe0a4be28ca07
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-02-22 10:39:26 +0330

    EXERCISE-LL-Unshift

--- js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Unshift.js
+++ js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Unshift.js
@@ -78,18 +78,12 @@ class LinkedList {
     return temp;
   }
 
-  unshift(value) {
-    const newNode = new Node(value)
-    if (!this.head) {
-      this.head = newNode
-      this.tail = newNode
-    } else {
-      newNode.next = this.head
-      this.head = newNode
-    }
-    this.length++
-    return this
-  }
+  /// WRITE UNSHIFT METHOD HERE ///
+  //                             //
+  //                             //
+  //                             //
+  //                             //
+  /////////////////////////////////
 
 }
 
