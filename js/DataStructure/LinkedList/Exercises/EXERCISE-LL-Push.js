a8dbfdaf2d071220627449f055290a2d2208eef2
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-03-03 17:12:08 +0330

    LL Push

--- js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.js
+++ js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.js
@@ -47,6 +47,24 @@ class LinkedList {
         this.length = 0;
     }
 
+    pop() {
+        if (!this.head) return undefined
+        let temp = this.head
+        let pre = this.head
+        while (temp.next) {
+            pre = temp
+            temp = temp.next
+        }
+        this.tail = pre
+        this.tail.next = null
+        this.length--
+        if (this.length === 0) {
+            this.head = null
+            this.tail = null
+        }
+        return temp
+    }
+
     push(value) {
         const newNode = new Node(value);
         if (!this.head) {
--- js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.test.js
+++ js/DataStructure/LinkedList/Exercises/EXERCISE-LL-Push.test.js
@@ -1,11 +1,62 @@
-import { describe, it, expect, vi, suite } from "vitest";
+import { describe } from "vitest";
+import { runTests } from "../../../utils";
 import LinkedList from "./EXERCISE-LL-Push";
-import { J } from "../../../utils";
 
-describe(`EXERCISE-LL-Push`, () => {
-  suite(`push()`, () => {
-    describe('\n\tlet myLinkedList = new LinkedList(7)\n\tmyLinkedList.push(4)\n\t', () => {
-      let myLinkedList = new LinkedList(7)
+describe('EXERCISE-LL-Push', () => {
+  describe(`push(value)`, () => {
+    describe('Before push(value)\n\tlet myLinkedList = new LinkedList();\n\tmyLinkedList.pop(); (length = 0)', () => {
+      let myLinkedList = new LinkedList()
+      myLinkedList.pop()
+
+      const tests = [
+        [
+          'myLinkedList.head', myLinkedList.head, 'toBeNull', null
+        ],
+        ['myLinkedList.head === myLinkedList.tail', myLinkedList.head === myLinkedList.tail, 'toBe', true],
+        [
+          'myLinkedList.tail', myLinkedList.tail, 'toBeNull', null
+        ],
+        ['myLinkedList.tail', myLinkedList.tail, 'toBeNull', null],
+        ['myLinkedList.length', myLinkedList.length, 'toEqual', 0],
+      ]
+
+      runTests(tests)
+    })
+
+    describe('After first push(value)\n\tlet myLinkedList = new LinkedList();\n\tmyLinkedList.push(7);', () => {
+      let myLinkedList = new LinkedList()
+      myLinkedList.pop()
+      const pushValue = myLinkedList.push(7)
+
+      const tests = [
+        [
+          'myLinkedList.head', myLinkedList.head, 'toEqual', {
+            value: 7,
+            next: null
+          }
+        ],
+        ['myLinkedList.head.next', myLinkedList.head.next, 'toBeNull', null],
+        ['myLinkedList.head.value', myLinkedList.head.value, 'toBe', 7],
+        [
+          'myLinkedList.tail', myLinkedList.tail, 'toEqual', {
+            value: 7,
+            next: null
+          }
+        ],
+        ['myLinkedList.tail.value', myLinkedList.tail.value, 'toBe', 7],
+        ['myLinkedList.tail.next', myLinkedList.tail.next, 'toBeNull', null],
+        ['pushValue === myLinkedList', pushValue === myLinkedList, 'toBe', true],
+        ['myLinkedList.head === myLinkedList.tail', myLinkedList.head === myLinkedList.tail, 'toBe', true],
+        ['myLinkedList.length', myLinkedList.length, 'toBe', 1],
+      ]
+
+      runTests(tests)
+    })
+
+    describe('After second push(value)\n\tlet myLinkedList = new LinkedList();\n\t myLinkedList.pop();\n\tmyLinkedList.push(7)\n\tmyLinkedList.push(4)', () => {
+      let myLinkedList = new LinkedList()
+      myLinkedList.pop()
+      myLinkedList.push(7)
       const pushValue = myLinkedList.push(4)
 
       const tests = [
@@ -18,32 +69,22 @@ describe(`EXERCISE-LL-Push`, () => {
             }
           }
         ],
-        [
-          'myLinkedList.head.next', myLinkedList.head.next, 'toEqual', {
-            value: 4,
-            next: null
-          }
-        ],
-        ['myLinkedList.head.value', myLinkedList.head.value, 'toEqual', 7],
-        ['myLinkedList.head.value', myLinkedList.head.value, 'toEqual', 7],
-        ['myLinkedList.head.next.value', myLinkedList.head.next.value, 'toEqual', 4],
         ['myLinkedList.head.next.next', myLinkedList.head.next.next, 'toBeNull', null],
+        ['myLinkedList.head.value', myLinkedList.head.value, 'toBe', 7],
         [
           'myLinkedList.tail', myLinkedList.tail, 'toEqual', {
             value: 4,
             next: null
           }
         ],
-        ['myLinkedList.tail.value', myLinkedList.tail.value, 'toEqual', 4],
+        ['myLinkedList.tail.value', myLinkedList.tail.value, 'toBe', 4],
         ['myLinkedList.tail.next', myLinkedList.tail.next, 'toBeNull', null],
-        ['myLinkedList.push(4) should return myLinkedList', pushValue, 'toEqual', myLinkedList],
+        ['pushValue === myLinkedList', pushValue === myLinkedList, 'toBe', true],
+        ['myLinkedList.length', myLinkedList.length, 'toBe', 2],
       ]
 
-      tests.forEach(([message, result, testMethod, expectedResult]) => {
-        it(`${message} >> ${J(expectedResult)}`, () => {
-          expect(result)[testMethod](expectedResult);
-        });
-      })
+      runTests(tests)
     })
+
   })
 })
