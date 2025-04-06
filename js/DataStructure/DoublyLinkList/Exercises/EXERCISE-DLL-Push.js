64d3bbe52c81dac72e4374b21bebb6d51244d073
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-06 19:55:24 +0330

    SOLUTION-DLL-Push

--- js/DataStructure/DoublyLinkList/Exercises/EXERCISE-DLL-Push.js
+++ js/DataStructure/DoublyLinkList/Exercises/EXERCISE-DLL-Push.js
@@ -1,59 +1,66 @@
 class Node {
-  constructor(value){
-      this.value = value;
-      this.next = null;
-      this.prev = null;
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
   }
 }
 
 class DoublyLinkedList {
   constructor(value) {
-      const newNode = new Node(value);
-      this.head = newNode;
-      this.tail = newNode;
-      this.length = 1;
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
   }
 
   printList() {
-      let temp = this.head;
-      while (temp !== null) {
-          console.log(temp.value);
-          temp = temp.next;
-      }
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
   }
 
   getHead() {
-      if (this.head === null) {
-          console.log("Head: null");
-      } else {
-          console.log("Head: " + this.head.value);
-      }
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
   }
 
   getTail() {
-      if (this.tail === null) {
-          console.log("Tail: null");
-      } else {
-          console.log("Tail: " + this.tail.value);
-      }
+    if (this.tail === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
   }
 
   getLength() {
-      console.log("Length: " + this.length);
+    console.log("Length: " + this.length);
   }
 
   makeEmpty() {
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-/// WRITE PUSH METHOD HERE ///
-//                          //
-//                          //
-//                          //
-//                          //
-//////////////////////////////
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 
 }
 
