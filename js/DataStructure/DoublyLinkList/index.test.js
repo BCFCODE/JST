7db3f0ee8f673fd53ffe5b93123b8fbb4d4ffa4a
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-07 16:52:44 +0330

    DLL Pop

--- js/DataStructure/DoublyLinkList/index.test.js
+++ js/DataStructure/DoublyLinkList/index.test.js
@@ -4,11 +4,38 @@ import { J, runTests } from "../../utils";
 
 describe(`DoublyLinkedList`, () => {
 
+  const propertyTests = (myDoublyLinkedList) => {
+    const nodeProperties = ['value', 'prev', 'next']
+    const headPropertyTests = nodeProperties.map(property =>
+      [
+        `\n\tmyDoublyLinkedList.head.hasOwnProperty(${property})`,
+        myDoublyLinkedList.head.hasOwnProperty(property), 'toBe', true
+      ]
+    )
+    const tailPropertyTests = nodeProperties.map(property =>
+      [
+        `\n\tmyDoublyLinkedList.tail.hasOwnProperty(${property})`,
+        myDoublyLinkedList.tail.hasOwnProperty(property), 'toBe', true
+      ]
+    )
+
+    const myDoublyLinkedListPropertyTests =
+      ['head', 'tail', 'length'].map(property =>
+        [
+          `\n\tmyDoublyLinkedList.hasOwnProperty(${property})`,
+          myDoublyLinkedList.hasOwnProperty(property), 'toBe', true
+        ]
+      )
+
+    return [...myDoublyLinkedListPropertyTests, ...headPropertyTests, ...tailPropertyTests]
+  }
+
   describe(`constructor`, () => {
     describe(`\n\tlet myDoublyLinkedList = new DoublyLinkedList(7)`, () => {
       let myDoublyLinkedList = new DoublyLinkedList(7)
 
       const tests = [
+        ...propertyTests(myDoublyLinkedList),
         [
           '\n\tmyDoublyLinkedList.head', myDoublyLinkedList.head, 'toEqual', {
             value: 7,
@@ -37,35 +64,81 @@ describe(`DoublyLinkedList`, () => {
 
     })
 
-    describe(`push()`, () => {
-      describe('\n\tlet myDoublyLinkedList = new DoublyLinkedList(1)\n\tmyDoublyLinkedList.push(2)', () => {
-        let myDoublyLinkedList = new DoublyLinkedList(1)
-        const pushValue = myDoublyLinkedList.push(2)
 
-        const headTests = [
-          ['\n\tmyDoublyLinkedList.head.value', myDoublyLinkedList.head.value, 'toBe', 1],
-          ['\n\tmyDoublyLinkedList.head.next.next', myDoublyLinkedList.head.prev, 'toBeNull', null],
-          ['\n\tmyDoublyLinkedList.head.next.value', myDoublyLinkedList.head.next.value, 'toBe', 2],
-          ['\n\tmyDoublyLinkedList.head.next.prev.value', myDoublyLinkedList.head.next.prev.value, 'toBe', 1],
-          ['\n\tmyDoublyLinkedList.head.next.prev.prev', myDoublyLinkedList.head.next.prev.prev, 'toBeNull', null],
-        ]
 
-        const tailTests = [
-          ['\n\tmyDoublyLinkedList.tail.value', myDoublyLinkedList.tail.value, 'toBe', 2],
-          ['\n\tmyDoublyLinkedList.tail.prev.value', myDoublyLinkedList.tail.prev.value, 'toBe', 1],
-          ['\n\tmyDoublyLinkedList.tail.prev.prev', myDoublyLinkedList.tail.prev.prev, 'toBeNull', null],
-          ['\n\tmyDoublyLinkedList.tail.prev.next.next', myDoublyLinkedList.tail.prev.next.next, 'toBeNull', null],
-        ]
+  })
 
-        const tests = [
-          ...headTests, ...tailTests,
-          ['myDoublyLinkedList.length', myDoublyLinkedList.length, 'toEqual', 2],
-          ['\n\tconst pushValue = myDoublyLinkedList.push(2); \n\tpushValue === myDoublyLinkedList', pushValue === myDoublyLinkedList, 'toBe', true]
-        ]
+  describe(`push()`, () => {
+    describe('\n\tlet myDoublyLinkedList = new DoublyLinkedList(1)\n\tmyDoublyLinkedList.push(2)', () => {
+      let myDoublyLinkedList = new DoublyLinkedList(1)
+      const pushValue = myDoublyLinkedList.push(2)
 
-        runTests(tests)
-      })
+      const headTests = [
+        ['\n\tmyDoublyLinkedList.head.value', myDoublyLinkedList.head.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.head.next.next', myDoublyLinkedList.head.prev, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.head.next.value', myDoublyLinkedList.head.next.value, 'toBe', 2],
+        ['\n\tmyDoublyLinkedList.head.next.prev.value', myDoublyLinkedList.head.next.prev.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.head.next.prev.prev', myDoublyLinkedList.head.next.prev.prev, 'toBeNull', null],
+      ]
 
+      const tailTests = [
+        ['\n\tmyDoublyLinkedList.tail.value', myDoublyLinkedList.tail.value, 'toBe', 2],
+        ['\n\tmyDoublyLinkedList.tail.prev.value', myDoublyLinkedList.tail.prev.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.tail.prev.prev', myDoublyLinkedList.tail.prev.prev, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.tail.prev.next.next', myDoublyLinkedList.tail.prev.next.next, 'toBeNull', null],
+      ]
+
+      const tests = [
+        ...propertyTests(myDoublyLinkedList),
+        ...headTests, ...tailTests,
+        ['myDoublyLinkedList.length', myDoublyLinkedList.length, 'toEqual', 2],
+        ['\n\tconst pushValue = myDoublyLinkedList.push(2); \n\tpushValue === myDoublyLinkedList', pushValue === myDoublyLinkedList, 'toBe', true]
+      ]
+
+      runTests(tests)
+    })
+
+  })
+
+  describe(`pop()`, () => {
+    describe('Before pop()\n\tlet myDoublyLinkedList = new LinkedList(1);\n\tmyDoublyLinkedList.push(2);', () => {
+      let myDoublyLinkedList = new DoublyLinkedList(1)
+      myDoublyLinkedList.push(2)
+
+      const tests = [
+        ...propertyTests(myDoublyLinkedList),
+        ['\n\tmyDoublyLinkedList.head.value', myDoublyLinkedList.head.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.tail.prev.value', myDoublyLinkedList.tail.prev.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.head.next.value', myDoublyLinkedList.head.next.value, 'toBe', 2],
+        ['\n\tmyDoublyLinkedList.tail.value', myDoublyLinkedList.tail.value, 'toBe', 2],
+        ['\n\tmyDoublyLinkedList.head.prev', myDoublyLinkedList.head.prev, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.tail.next', myDoublyLinkedList.tail.next, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.head.next.next', myDoublyLinkedList.head.next.next, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.tail.prev.prev', myDoublyLinkedList.tail.prev.prev, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.length', myDoublyLinkedList.length, 'toBe', 2],
+      ]
+
+      runTests(tests)
+    })
+
+    describe('After pop()\n\tlet myDoublyLinkedList = new LinkedList(1);\n\tmyDoublyLinkedList.push(2);\n\tconst popValue = myDoublyLinkedList.pop()', () => {
+      let myDoublyLinkedList = new DoublyLinkedList(1)
+      myDoublyLinkedList.push(2)
+      const popValue = myDoublyLinkedList.pop()
+      // console.log(myDoublyLinkedList)
+
+      const tests = [
+        ...propertyTests(myDoublyLinkedList),
+        ['\n\tmyDoublyLinkedList.head.value', myDoublyLinkedList.head.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.head.next', myDoublyLinkedList.head.next, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.head.prev', myDoublyLinkedList.head.prev, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.tail.value', myDoublyLinkedList.tail.value, 'toBe', 1],
+        ['\n\tmyDoublyLinkedList.tail.next', myDoublyLinkedList.tail.next, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.tail.prev', myDoublyLinkedList.tail.prev, 'toBeNull', null],
+        ['\n\tmyDoublyLinkedList.length', myDoublyLinkedList.length, 'toBe', 1],
+      ]
+
+      runTests(tests)
     })
   })
 
