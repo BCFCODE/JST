ddeb18627514cbf248fe09911e4e787a9a7c3110
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-20 18:09:11 +0330

    SOLUTION-DLL-Unshift

--- js/DataStructure/DoublyLinkList/Exercises/Unshift/EXERCISE-DLL-Unshift.js
+++ js/DataStructure/DoublyLinkList/Exercises/Unshift/EXERCISE-DLL-Unshift.js
@@ -77,12 +77,19 @@ class DoublyLinkedList {
     return temp;
   }
 
-  /// WRITE UNSHIFT METHOD HERE ///
-  //                             //
-  //                             //
-  //                             //
-  //                             //
-  /////////////////////////////////
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 
 }
 
