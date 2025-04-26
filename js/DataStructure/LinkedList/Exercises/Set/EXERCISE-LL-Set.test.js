526bdfce001bd8504d27c323d64c4a017a2fc759
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-26 14:42:16 +0330

    LL Set

--- js/DataStructure/LinkedList/Exercises/Set/EXERCISE-LL-Set.test.js
+++ js/DataStructure/LinkedList/Exercises/Set/EXERCISE-LL-Set.test.js
@@ -1,6 +1,7 @@
 import { describe } from "vitest";
 import { executeTestCases } from "../../../../utils";
 import LinkedList from "./EXERCISE-LL-Set";
+import { afterSet4ToIndex1, beforeSet, outOfRangeIndex } from "../../TESTS/Set";
 
 describe(`EXERCISE-LL-Set`, () => {
 
@@ -10,126 +11,25 @@ describe(`EXERCISE-LL-Set`, () => {
     myLinkedList.push(23)
     myLinkedList.push(7)
 
-    const tests = [
-      [
-        'myLinkedList.head', myLinkedList.head, 'toEqual', {
-          value: 11,
-          next: {
-            value: 3,
-            next: {
-              value: 23,
-              next: {
-                value: 7,
-                next: null
-              }
-            }
-          }
-        }
-      ],
-      [
-        'myLinkedList.head.next', myLinkedList.head.next, 'toEqual', {
-          value: 3,
-          next: {
-            value: 23,
-            next: {
-              value: 7,
-              next: null
-            }
-          }
-        }
-      ],
-      ['myLinkedList.head.value', myLinkedList.head.value, 'toBe', 11],
-      ['myLinkedList.head.next.next.next.next', myLinkedList.head.next.next.next.next, 'toBeNull', null],
-      ['myLinkedList.head.next.next.next === myLinkedList.tail', myLinkedList.head.next.next.next === myLinkedList.tail, 'toBe', true],
-      [
-        'myLinkedList.tail', myLinkedList.tail, 'toEqual', {
-          value: 7,
-          next: null
-        }
-      ],
-      ['myLinkedList.tail.value', myLinkedList.tail.value, 'toBe', 7],
-      ['myLinkedList.tail.next', myLinkedList.tail.next, 'toBeNull', null],
-      ['myLinkedList.length', myLinkedList.length, 'toEqual', 4],
-    ]
+    const tests = beforeSet(myLinkedList)
 
     executeTestCases(tests)
   })
 
-  describe('After set(index, value)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.push(23);\n\tmyLinkedList.push(7);\n\tmyLinkedList.set(1, 4)', () => {
+  describe('After set(1, 4)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.push(23);\n\tmyLinkedList.push(7);\n\tmyLinkedList.set(1, 4)', () => {
     let myLinkedList = new LinkedList(11)
     myLinkedList.push(3)
     myLinkedList.push(23)
     myLinkedList.push(7)
     const setValue = myLinkedList.set(1, 4)
 
-    const tests = [
-      [
-        'myLinkedList.head', myLinkedList.head, 'toEqual', {
-          value: 11,
-          next: {
-            value: 4,
-            next: {
-              value: 23,
-              next: {
-                value: 7,
-                next: null
-              }
-            }
-          }
-        }
-      ],
-      [
-        'myLinkedList.head.next', myLinkedList.head.next, 'toEqual', {
-          value: 4,
-          next: {
-            value: 23,
-            next: {
-              value: 7,
-              next: null
-            }
-          }
-        }
-      ],
-      ['myLinkedList.head.value', myLinkedList.head.value, 'toBe', 11],
-      ['myLinkedList.head.next.next.next.next', myLinkedList.head.next.next.next.next, 'toBeNull', null],
-      ['myLinkedList.head.next.next.next.next === myLinkedList.tail', myLinkedList.head.next.next.next === myLinkedList.tail, 'toBe', true],
-      [
-        'myLinkedList.tail', myLinkedList.tail, 'toEqual', {
-          value: 7,
-          next: null
-        }
-      ],
-      ['myLinkedList.tail.value', myLinkedList.tail.value, 'toBe', 7],
-      ['myLinkedList.tail.next', myLinkedList.tail.next, 'toBeNull', null],
-      ['myLinkedList.length', myLinkedList.length, 'toEqual', 4],
-      ['\n\tconst setValue = myLinkedList.set(1, 4);\n\tsetValue === true >> true', setValue === true, 'toBe', true],
-    ]
+    const tests = afterSet4ToIndex1(myLinkedList, setValue)
 
     executeTestCases(tests)
   })
 
-  describe('After set(index, value)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.set(-1, 4)', () => {
-    let myLinkedList = new LinkedList(11)
-    myLinkedList.push(3)
-    const setValue = myLinkedList.set(-1, 4)
+});
 
-    const tests = [
-      ['\n\tconst setValue = myLinkedList.set(-1, 4);\n\tsetValue === false >> true', setValue === false, 'toBe', true],
-    ]
+// Out of range tests
+[-1, 2].forEach(index => outOfRangeIndex(LinkedList, index))
 
-    executeTestCases(tests)
-  })
-
-  describe('After set(index, value)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.set(5, 4)', () => {
-    let myLinkedList = new LinkedList(11)
-    myLinkedList.push(3)
-    const setValue = myLinkedList.set(5, 4)
-
-    const tests = [
-      ['\n\tconst setValue = myLinkedList.set(-1, 4);\n\tsetValue === false >> true', setValue === false, 'toBe', true],
-    ]
-
-    executeTestCases(tests)
-  })
-
-})
--- js/DataStructure/LinkedList/index.test.js
+++ js/DataStructure/LinkedList/index.test.js
@@ -1,6 +1,7 @@
 import { LinkedList } from ".";
 import { describe, it, expect, describe } from "vitest";
 import { J, executeTestCases } from "../../utils";
+import { beforeSet } from "./TESTS/Set";
 
 describe(`LinkedList`, () => {
 
@@ -509,58 +510,18 @@ describe(`LinkedList`, () => {
   })
 
   describe(`set(index, value)`, () => {
-    describe('Before set(index, value)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.push(23);\n\tmyLinkedList.push(7);', () => {
+    describe('Before set()\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.push(23);\n\tmyLinkedList.push(7);', () => {
       let myLinkedList = new LinkedList(11)
       myLinkedList.push(3)
       myLinkedList.push(23)
       myLinkedList.push(7)
 
-      const tests = [
-        [
-          'myLinkedList.head', myLinkedList.head, 'toEqual', {
-            value: 11,
-            next: {
-              value: 3,
-              next: {
-                value: 23,
-                next: {
-                  value: 7,
-                  next: null
-                }
-              }
-            }
-          }
-        ],
-        [
-          'myLinkedList.head.next', myLinkedList.head.next, 'toEqual', {
-            value: 3,
-            next: {
-              value: 23,
-              next: {
-                value: 7,
-                next: null
-              }
-            }
-          }
-        ],
-        ['myLinkedList.head.value', myLinkedList.head.value, 'toBe', 11],
-        ['myLinkedList.head.next.next.next.next', myLinkedList.head.next.next.next.next, 'toBeNull', null],
-        ['myLinkedList.head.next.next.next === myLinkedList.tail', myLinkedList.head.next.next.next === myLinkedList.tail, 'toBe', true],
-        [
-          'myLinkedList.tail', myLinkedList.tail, 'toEqual', {
-            value: 7,
-            next: null
-          }
-        ],
-        ['myLinkedList.tail.value', myLinkedList.tail.value, 'toBe', 7],
-        ['myLinkedList.tail.next', myLinkedList.tail.next, 'toBeNull', null],
-        ['myLinkedList.length', myLinkedList.length, 'toEqual', 4],
-      ]
+      const tests = beforeSet(myLinkedList)
 
       executeTestCases(tests)
     })
 
-    describe('After set(index, value)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.push(23);\n\tmyLinkedList.push(7);\n\tmyLinkedList.set(1, 4)', () => {
+    describe('After set(1, 4)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.push(23);\n\tmyLinkedList.push(7);\n\tmyLinkedList.set(1, 4)', () => {
       let myLinkedList = new LinkedList(11)
       myLinkedList.push(3)
       myLinkedList.push(23)
@@ -613,8 +574,8 @@ describe(`LinkedList`, () => {
       executeTestCases(tests)
     })
 
-    describe('After set(index, value)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.set(-1, 4)', () => {
-      let myLinkedList = new LinkedList(11)
+    describe('After set(-1, 4)\n\tlet myLinkedList = new LinkedList(11);\n\tmyLinkedList.push(3);\n\tmyLinkedList.set(-1, 4)', () => {
+      let myLinkedList = new LinkedList(11) 
       myLinkedList.push(3)
       const setValue = myLinkedList.set(-1, 4)
 
