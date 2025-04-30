042055c35b3f19706fbdee2dc79a48cc7a2ad82d
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-30 12:29:06 +0330

    EXERCISE-DLL-Pop

--- js/DataStructure/DoublyLinkList/Exercises/Pop/EXERCISE-DLL-Pop.js
+++ js/DataStructure/DoublyLinkList/Exercises/Pop/EXERCISE-DLL-Pop.js
@@ -62,20 +62,12 @@ class DoublyLinkedList {
     return this;
   }
 
-  pop() {
-    if (this.length === 0) return undefined;
-    let temp = this.tail;
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.tail = this.tail.prev;
-      this.tail.next = null;
-      temp.prev = null;
-    }
-    this.length--;
-    return temp;
-  }
+  /// WRITE POP METHOD HERE ///
+  //                         //
+  //                         //
+  //                         //
+  //                         //
+  /////////////////////////////
 
 }
 
