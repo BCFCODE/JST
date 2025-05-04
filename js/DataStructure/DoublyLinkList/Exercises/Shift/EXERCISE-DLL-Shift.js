3069e207f4c7cad6daddc5fabccf488220f68759
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-05-04 18:12:36 +0330

    SOLUTION-DLL-Shift

--- js/DataStructure/DoublyLinkList/Exercises/Shift/EXERCISE-DLL-Shift.js
+++ js/DataStructure/DoublyLinkList/Exercises/Shift/EXERCISE-DLL-Shift.js
@@ -91,12 +91,20 @@ class DoublyLinkedList {
     return this;
   }
 
-  /// WRITE SHIFT METHOD HERE ///
-  //                           //
-  //                           //
-  //                           //
-  //                           //
-  ///////////////////////////////
+  shift() {
+    if (this.length === 0) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
 
 }
 
