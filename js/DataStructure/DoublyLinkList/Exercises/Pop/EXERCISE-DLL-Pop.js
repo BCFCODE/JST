6e677b6eb34efa082487a4b13fe8c022a2727656
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-05-10 11:27:14 +0330

    EXERCISE-DLL-Pop

--- js/DataStructure/DoublyLinkList/Exercises/Pop/EXERCISE-DLL-Pop.js
+++ js/DataStructure/DoublyLinkList/Exercises/Pop/EXERCISE-DLL-Pop.js
@@ -1,121 +1,113 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-    this.prev = null;
-  }
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
 }
-
+ 
 class DoublyLinkedList {
-  constructor(value) {
-    const newNode = new Node(value);
-    this.head = newNode;
-    this.tail = newNode;
-    this.length = 1;
-  }
-
-  printList() {
-    let temp = this.head;
-    while (temp !== null) {
-      console.log(temp.value);
-      temp = temp.next;
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
     }
-  }
 
-  getHead() {
-    if (this.head === null) {
-      console.log("Head: null");
-    } else {
-      console.log("Head: " + this.head.value);
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
     }
-  }
 
-  getTail() {
-    if (this.tail === null) {
-      console.log("Tail: null");
-    } else {
-      console.log("Tail: " + this.tail.value);
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
     }
-  }
 
-  getLength() {
-    console.log("Length: " + this.length);
-  }
-
-  makeEmpty() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  push(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
     }
-    this.length++;
-    return this;
-  }
 
-  pop() {
-    if (this.length === 0) return undefined;
-    let temp = this.tail;
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.tail = this.tail.prev;
-      this.tail.next = null;
-      temp.prev = null;
+    getLength() {
+        console.log("Length: " + this.length);
     }
-    this.length--;
-    return temp;
-  }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ 
+    push(value){
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+	/// WRITE POP METHOD HERE ///
+	//                         //
+	//                         //
+	//                         //
+	//                         //
+	/////////////////////////////
 
 }
 
 
 function test() {
-  let myDLL = new DoublyLinkedList(1);
-  myDLL.push(2);
+    let myDLL = new DoublyLinkedList(1);
+    myDLL.push(2);
 
-  // (2) Items in LL - Returns 2 Node
-  if (myDLL.length !== 0) {
-    console.log(myDLL.pop().value);
-  } else {
-    console.log("null");
-  }
+    // (2) Items in LL - Returns 2 Node
+    if (myDLL.length !== 0) {
+        console.log(myDLL.pop().value);
+    } else {
+        console.log("null");
+    }
 
-  // (1) Item in LL - Returns 1 Node
-  if (myDLL.length !== 0) {
-    console.log(myDLL.pop().value);
-  } else {
-    console.log("null");
-  }
+    // (1) Item in LL - Returns 1 Node
+    if (myDLL.length !== 0) {
+        console.log(myDLL.pop().value);
+    } else {
+        console.log("null");
+    }
 
-  // (0) Items in LL - Returns null
-  if (myDLL.length !== 0) {
-    console.log(myDLL.pop().value);
-  } else {
-    console.log("null");
-  }
+    // (0) Items in LL - Returns null
+    if (myDLL.length !== 0) {
+        console.log(myDLL.pop().value);
+    } else {
+        console.log("null");
+    }
 }
 
 
 test();
-
-
+ 
+    
 /*
-  EXPECTED OUTPUT:
-  ----------------
-  2
-  1
-  null
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    null
 
 */
 
