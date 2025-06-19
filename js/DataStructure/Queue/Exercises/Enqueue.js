b4a5d5d37c4fdafb7b90d7db3c04b5d41846787b
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-19 10:26:34 +0330

    SOLUTION-Queue-Enqueue

--- js/DataStructure/Queue/Exercises/Enqueue.js
+++ js/DataStructure/Queue/Exercises/Enqueue.js
@@ -13,12 +13,19 @@ class Queue {
     this.length = 1
   }
 
-  // WRITE ENQUEUE METHOD HERE //
-  //                           //
-  //                           //
-  //                           //
-  //                           //
-  ///////////////////////////////
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this
+  }
+
 }
 
 export default Queue
