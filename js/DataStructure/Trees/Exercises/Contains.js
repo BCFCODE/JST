75b5da5a1a5f234d814cad3b1e0938c9d859c722
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-20 19:21:39 +0330

    EXERCISE-BST-Contains

---
+++ js/DataStructure/Trees/Exercises/Contains.js
@@ -0,0 +1,48 @@
+export class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode
+      return this
+    }
+    let temp = this.root
+    while (true) {
+      if (newNode === temp) return undefined
+      if (newNode.value === temp.value) return undefined
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode
+          return this
+        }
+        temp = temp.left
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode
+          return this
+        }
+        temp = temp.right
+      }
+    }
+  }
+
+  /// WRITE CONTAINS METHOD HERE ///
+  //                              //
+  //                              //
+  //                              //
+  //                              //
+  //////////////////////////////////
+}
+
+export default BST
