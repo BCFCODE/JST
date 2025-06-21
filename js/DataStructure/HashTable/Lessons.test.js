552fc9e6941fabc52ba24fb3801e7b39b777ec6b
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-21 11:33:58 +0330

    HT Constructor

---
+++ js/DataStructure/HashTable/Lessons.test.js
@@ -0,0 +1,8 @@
+import { describe } from "vitest";
+import HashTable from "./Lessons";
+import { constructorTests } from "./tests";
+
+describe(`HashTable`, () => {
+  constructorTests(HashTable)
+})
+
