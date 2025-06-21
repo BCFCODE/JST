0beb05e41d6cf916676df224b923cc9f617e4171
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-21 20:13:53 +0330

    BST Get (Lessons)

---
+++ js/DataStructure/HashTable/tests/Get.js
@@ -0,0 +1,45 @@
+import { describe } from "vitest"
+import { checkHTInternalStructure, executeTestCases, J } from "../../../utils"
+import CorrectHashTable from "./Correct"
+import { toEqualDataMapTests } from "./dataMapTests"
+
+export const validateGetOperations = ({ myHashTable, returnValue, key }) => {
+  let correct = new CorrectHashTable()
+  correct.set('bolts', 1400)
+  correct.set('washers', 50)
+  const correctReturnValue = correct.get(key)
+
+  return [
+    checkHTInternalStructure(myHashTable),
+    toEqualDataMapTests({ myHashTable, correct }),
+    [
+      ['\n\treturnValue', returnValue, 'toEqual', correctReturnValue]
+    ]
+  ].flat()
+}
+
+const getTests = (HashTable) => {
+  describe(`set`, () => {
+
+    ['bolts', 'washers', 'lumber'].forEach(key => {
+
+      describe(`\n\tlet myHashTable = new HashTable()\n\t myHashTable.set('bolts', 1400)\n\tmyHashTable.set('washers', 50)\n\tconst returnValue = myHashTable.get(${J(key)})`, () => {
+
+        let myHashTable = new HashTable()
+        myHashTable.set('bolts', 1400)
+        myHashTable.set('washers', 50)
+        const returnValue = myHashTable.get(key)
+
+        const tests = validateGetOperations({ myHashTable, returnValue, key })
+
+        executeTestCases(tests)
+      })
+
+    })
+
+  })
+}
+
+export default getTests
+
+
