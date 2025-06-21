0319fdf9d3e764b3d2d00030d97821f28a0ac915
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-21 15:10:49 +0330

    EXERCISE-BST-Insert

--- js/DataStructure/Trees/Exercises/Insert.js
+++ js/DataStructure/Trees/Exercises/Insert.js
@@ -11,31 +11,12 @@ class BST {
     this.root = null
   }
 
-  insert(value) {
-    const newNode = new Node(value);
-    if (this.root === null) {
-      this.root = newNode
-      return this
-    }
-    let temp = this.root
-    while (true) {
-      if (newNode === temp) return undefined
-      if (newNode.value === temp.value) return undefined
-      if (newNode.value < temp.value) {
-        if (temp.left === null) {
-          temp.left = newNode
-          return this
-        }
-        temp = temp.left
-      } else {
-        if (temp.right === null) {
-          temp.right = newNode
-          return this
-        }
-        temp = temp.right
-      }
-    }
-  }
+ 	/// WRITE INSERT METHOD HERE ///
+	//                            //
+	//                            //
+	//                            //
+	//                            //
+	////////////////////////////////
 }
 
 export default BST
