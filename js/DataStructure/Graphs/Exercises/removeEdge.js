81b898d3feee67c7e235fcfd2c16948dd9cdc291
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-22 16:50:32 +0330

    SOLUTION-GR-removeEdge

--- js/DataStructure/Graphs/Exercises/removeEdge.js
+++ js/DataStructure/Graphs/Exercises/removeEdge.js
@@ -20,12 +20,16 @@ class Graph {
     return false
   }
 
-  /// WRITE removeEdge METHOD HERE ///
-  //                                //
-  //                                //
-  //                                //
-  //                                //
-  ////////////////////////////////////
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1]
+        .filter(v => v !== vertex2)
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2]
+        .filter(v => v !== vertex1)
+      return true
+    }
+    return false
+  }
 }
 
 export default Graph
