a61a6350449c874d6bb11df1e07f3e43405016d9
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-22 17:04:26 +0330

    EXERCISE-GR-addEdge

--- js/DataStructure/Graphs/Exercises/addEdge.js
+++ js/DataStructure/Graphs/Exercises/addEdge.js
@@ -11,14 +11,12 @@ class Graph {
     return false
   }
 
-  addEdge(vertex1, vertex2) {
-    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1].push(vertex2)
-      this.adjacencyList[vertex2].push(vertex1)
-      return true
-    }
-    return false
-  }
+  /// WRITE addEdge METHOD HERE ///
+  //                             //
+  //                             //
+  //                             //
+  //                             //
+  /////////////////////////////////
 }
 
 export default Graph
