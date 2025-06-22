3c7f1417aea9feeac14c9c418fc3ed0d5b994bf5
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-22 17:00:14 +0330

    EXERCISE-GR-addVertex

--- js/DataStructure/Graphs/Exercises/addVertex.js
+++ js/DataStructure/Graphs/Exercises/addVertex.js
@@ -1,15 +1,14 @@
 class Graph {
   constructor() {
-    this.adjacencyList = {}
+    //...
   }
 
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = []
-      return true
-    }
-    return false
-  }
+  /// WRITE addVertex METHOD HERE ///
+  //                               //
+  //                               //
+  //                               //
+  //                               //
+  ///////////////////////////////////
 }
 
 export default Graph
