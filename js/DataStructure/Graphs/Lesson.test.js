1b708916bde5e87707fcd88c44e0ebb6345bb5c6
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-22 14:17:40 +0330

    GR addEdge (Lessons)

--- js/DataStructure/Graphs/Lesson.test.js
+++ js/DataStructure/Graphs/Lesson.test.js
@@ -2,10 +2,12 @@ import { describe } from "vitest";
 import Graph from "./Lessons";
 
 import {
+  addEdgeTests,
   addVertexTests
 } from "./tests";
 
 describe(`Graphs`, () => {
   addVertexTests(Graph)
+  addEdgeTests(Graph)
 })
 
