7c3e2120ec9af22df32b9e05463d44e4548dae26
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-23 19:29:05 +0330

    EXERCISE-Heap-remove() (without #sinkDown)

--- js/DataStructure/Heap/Exercises/remove.js
+++ js/DataStructure/Heap/Exercises/remove.js
@@ -54,19 +54,5 @@ export class Heap {
     }
   }
 
-  remove() {
-    if (this.#heap.length === 0) {
-      return null
-    }
-
-    if (this.#heap.length === 1) {
-      return this.#heap.pop()
-    }
-
-    const maxValue = this.#heap[0]
-    this.#heap[0] = this.#heap.pop()
-    this.#sinkDown(0)
-
-    return maxValue
-  }
+  // ...
 }
