30f9324e452c98e9295c545e6fd5e7c251bdeb69
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-23 14:47:07 +0330

    EXERCISE-Heap-#leftChild(index)

--- js/DataStructure/Heap/Exercises/index.js
+++ js/DataStructure/Heap/Exercises/index.js
@@ -1,2 +1,3 @@
 export { Heap as HeapClassHelperMethodHeap } from "./helper/heap";
 export { Heap as HeapClassHelperMethodGetHeap } from "./helper/getHeap";
+export { Heap as HeapClassHelperMethodLeftChild } from "./helper/leftChild";
