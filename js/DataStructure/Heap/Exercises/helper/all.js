b2ed944c4c411974d0bc48b063cf20dfa2cc3059
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-24 12:29:55 +0330

    EXERCISE-Heap-all

--- js/DataStructure/Heap/Exercises/helper/all.js
+++ js/DataStructure/Heap/Exercises/helper/all.js
@@ -1,25 +1,15 @@
 export class Heap {
-  #heap = []
+  // ...
 
-  getHeap() {
-    return [...this.#heap]
-  }
+  // ...
 
-  #leftChild(index) {
-    return 2 * index + 1
-  }
+  // ...
 
-  #rightChild(index) {
-    return 2 * index + 2
-  }
+  // ...
 
-  #parent(index) {
-    return Math.floor((index - 1) / 2)
-  }
+  // ...
 
-  #swap(index1, index2) {
-    [this.#heap[index1], this.#heap[index2]] = [this.#heap[index2], this.#heap[index1]]
-  }
+  // ...
 
   #sinkDown(index) {
     let maxIndex = index
