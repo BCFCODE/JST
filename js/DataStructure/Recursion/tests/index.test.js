888f6c4e59b803e72049476c7b92f4830f511e91
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-06-24 14:20:24 +0330

    Algorithms Recursion (Lessons)

---
+++ js/DataStructure/Recursion/tests/index.test.js
@@ -0,0 +1,23 @@
+import { describe } from "vitest";
+import recursionTests from ".";
+import { factorial as factorialFn } from "../Lessons";
+import { factorialFnExercise } from "../Exercises";
+
+const tests = [
+  {
+    description: 'EXERCISE-Recursion-factorial(n) (Lessons)',
+    fn: factorialFn
+  },
+  {
+    description: 'EXERCISE-Recursion-factorial(n)',
+    fn: factorialFn
+  },
+]
+
+tests.forEach(({ description, fn }) => {
+  describe(description, () => recursionTests(fn))
+})
+
+
+
+
