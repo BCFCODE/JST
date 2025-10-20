ec900ba22be9e25b8cd45ddf6cdb1fb7d1c8e306
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-10-20 23:55:51 +0330

    SOLUTION-Recursion-deleteNode

--- js/DataStructure/BST/Exercises/deleteNode.js
+++ js/DataStructure/BST/Exercises/deleteNode.js
@@ -1,88 +1,116 @@
 export class Node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class BST {
   constructor() {
-    this.root = null
+    this.root = null;
   }
 
   insert(value) {
     const newNode = new Node(value);
     if (this.root === null) {
-      this.root = newNode
-      return this
+      this.root = newNode;
+      return this;
     }
-    let temp = this.root
+    let temp = this.root;
     while (true) {
-      if (newNode === temp) return undefined
-      if (newNode.value === temp.value) return undefined
+      if (newNode === temp) return undefined;
+      if (newNode.value === temp.value) return undefined;
       if (newNode.value < temp.value) {
         if (temp.left === null) {
-          temp.left = newNode
-          return this
+          temp.left = newNode;
+          return this;
         }
-        temp = temp.left
+        temp = temp.left;
       } else {
         if (temp.right === null) {
-          temp.right = newNode
-          return this
+          temp.right = newNode;
+          return this;
         }
-        temp = temp.right
+        temp = temp.right;
       }
     }
   }
 
   contains(value) {
-    if (this.root === null) return false
-    let temp = this.root
+    if (this.root === null) return false;
+    let temp = this.root;
     while (temp) {
       if (value < temp.value) {
-        temp = temp.left
+        temp = temp.left;
       } else if (value > temp.value) {
-        temp = temp.right
+        temp = temp.right;
       } else {
-        return true
+        return true;
       }
     }
-    return false
+    return false;
   }
 
   rContains(value, currentNode = this.root) {
-    if (currentNode === null) return false
+    if (currentNode === null) return false;
 
-    if (value === currentNode.value) return true
+    if (value === currentNode.value) return true;
 
     if (value < currentNode.value) {
-      return this.rContains(value, currentNode.left)
+      return this.rContains(value, currentNode.left);
     } else {
-      return this.rContains(value, currentNode.right)
+      return this.rContains(value, currentNode.right);
     }
   }
 
-
   #rInsert(value, currentNode = this.root) {
-    if (currentNode === null) return new Node(value)
+    if (currentNode === null) return new Node(value);
 
     if (value < currentNode.value) {
-      currentNode.left = this.#rInsert(value, currentNode.left)
+      currentNode.left = this.#rInsert(value, currentNode.left);
     } else if (value > currentNode.value) {
-      currentNode.right = this.#rInsert(value, currentNode.right)
+      currentNode.right = this.#rInsert(value, currentNode.right);
     }
 
-    return currentNode
+    return currentNode;
   }
 
   rInsert(value) {
-    if (this.root === null) this.root = new Node(value)
-    this.#rInsert(value)
+    if (this.root === null) this.root = new Node(value);
+    this.#rInsert(value);
   }
 
-  // ...
+  minValue(currentNode) {
+    while (currentNode.left != null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+
+  #deleteNode(value, currentNode) {
+    if (currentNode === null) return null;
+
+    if (value < currentNode.value) {
+      currentNode.left = this.#deleteNode(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.#deleteNode(value, currentNode.right);
+    } else {
+      if (currentNode.left === null && currentNode.right === null) {
+        return null;
+      } else if (currentNode.left === null) {
+        currentNode = currentNode.right;
+      } else if (currentNode.right === null) {
+        currentNode = currentNode.left;
+      }
+    }
+
+    return currentNode;
+  }
+
+  deleteNode(value) {
+    this.root = this.#deleteNode(value, this.root);
+  }
 }
 
-export default BST
+export default BST;
