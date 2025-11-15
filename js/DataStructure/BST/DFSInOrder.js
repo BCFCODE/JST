e13b95c5008aa8e90a87cc19bfc1fa285d8db010
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-11-15 15:06:09 +0330

    SOLUTION-BST-DFSInOrder

--- js/DataStructure/BST/DFSInOrder.js
+++ js/DataStructure/BST/DFSInOrder.js
@@ -151,7 +151,16 @@ class BST {
     return results;
   }
 
-  // ...
+  DFSInOrder() {
+    const results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
 
 export default BST;
