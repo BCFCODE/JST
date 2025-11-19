aa5d41bb64158070f2ee812ba7eaa1afb46208e8
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-11-19 16:01:24 +0330

    EXERCISE-SelectionSort

--- js/DataStructure/Sort/index.test.js
+++ js/DataStructure/Sort/index.test.js
@@ -1,12 +1,18 @@
 import { describe, it, expect } from "vitest";
 import { bubbleSort } from "./bubbleSort";
+import { selectionSort } from "./selectionSort";
 
 const test = [4, 2, 6, 5, 1, 3];
 const expectedResult = [1, 2, 3, 4, 5, 6];
 
-describe(`Bubble Sort`, () => {
+describe(`Sort`, () => {
   it(`\n\tbubbleSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
     const result = bubbleSort(test);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it(`\n\tselectionSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
+    const result = selectionSort(test);
     expect(result).toEqual(expectedResult);
   });
 });
