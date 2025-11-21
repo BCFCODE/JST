d603058f2ce4f742af6b2d9a15de6363b7d61ade
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-11-21 15:43:34 +0330

    SOLUTION-InsertionSort

--- js/DataStructure/Sort/insertionSort.js
+++ js/DataStructure/Sort/insertionSort.js
@@ -1,3 +1,13 @@
-// Do your magic here...
+function insertionSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
 
 export { insertionSort };
