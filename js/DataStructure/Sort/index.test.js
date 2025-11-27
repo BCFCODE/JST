080e246d21d9a1f3e1a603aa4e2bd84acc2d2074
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-11-27 16:25:09 +0330

    EXERCISE-SelectionSort

--- js/DataStructure/Sort/index.test.js
+++ js/DataStructure/Sort/index.test.js
@@ -4,17 +4,17 @@ import { selectionSort } from "./selectionSort";
 import { insertionSort } from "./insertionSort";
 
 describe(`Sort`, () => {
-  it(`\n\tbubbleSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
+  it(`bubbleSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
     const result = bubbleSort([4, 2, 6, 5, 1, 3]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
 
-  it(`\n\tselectionSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
+  it(`selectionSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
     const result = selectionSort([4, 2, 6, 5, 1, 3]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
 
-  it(`\n\tinsertionSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
+  it(`insertionSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
     const result = insertionSort([4, 2, 6, 5, 1, 3]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
