41ac02dc1d73d88e6f654e82f2e8fa10a962c31d
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-11-29 20:42:18 +0330

    EXERCISE-Merge

--- js/DataStructure/Sort/index.test.js
+++ js/DataStructure/Sort/index.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest";
 import { bubbleSort } from "./bubbleSort";
 import { selectionSort } from "./selectionSort";
 import { insertionSort } from "./insertionSort";
+import { merge } from "./merge";
 
 describe(`Sort`, () => {
   it(`bubbleSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
@@ -17,5 +18,10 @@ describe(`Sort`, () => {
   it(`insertionSort([4, 2, 6, 5, 1, 3]) >> [1, 2, 3, 4, 5, 6]`, () => {
     const result = insertionSort([4, 2, 6, 5, 1, 3]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it(`merge([1, 3, 7, 8], [2, 4, 5, 6]) >> [1, 2, 3, 4, 5, 6, 7, 8]`, () => {
+    const result = merge([1, 3, 7, 8], [2, 4, 5, 6]);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
