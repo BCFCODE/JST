f99af81f40df7396bced2c4f7704e586b973671b
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-09 11:36:29 +0330

    SOLUTION > BFS Code

--- js/DataStructure/BST/BFS.js
+++ js/DataStructure/BST/BFS.js
@@ -115,8 +115,19 @@ class BST {
     this.root = this.#deleteNode(value, this.root);
   }
 
-  // ...
-  
+  BFS() {
+    let currentNode = this.root;
+    let queue = [];
+    let results = [];
+    queue.push(currentNode);
+    while (queue.length) {
+      currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return results;
+  }
 }
 
 export default BST;
