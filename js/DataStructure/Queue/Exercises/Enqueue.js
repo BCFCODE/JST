94c70aed8ab9a2b46ab07c324390bea73ca22144
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-19 09:47:17 +0330

    EXERCISE > Queue Enqueue

--- js/DataStructure/Queue/Exercises/Enqueue.js
+++ js/DataStructure/Queue/Exercises/Enqueue.js
@@ -1,30 +1,9 @@
-class Node {
-  constructor(value) {
-    this.value = value
-    this.next = null
-  }
-}
+// ...
 
 export class Queue {
-  constructor(value) {
-    const newNode = new Node(value)
-    this.first = newNode
-    this.last = newNode
-    this.length = 1
-  }
+  // ...
 
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    this.length++;
-    return this
-  }
+  // ...
 
 }
 
