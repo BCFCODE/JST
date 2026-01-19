6591d1690a3accb32dc76e9e0ee8721fc6c845a0
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-19 10:25:46 +0330

    SOLUTION > Quick Sort Code

--- js/DataStructure/Sort/quickSort.js
+++ js/DataStructure/Sort/quickSort.js
@@ -16,6 +16,13 @@ function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
   return swapIndex;
 }
 
-// ...
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right);
+    quickSort(array, left, pivotIndex - 1);
+    quickSort(array, pivotIndex + 1, right);
+  }
+  return array;
+}
 
 export { swap, pivot, quickSort };
