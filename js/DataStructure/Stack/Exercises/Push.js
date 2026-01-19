6b6c61389580a5ca7c32ce9a2057d0a0abf39f73
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-19 09:33:54 +0330

    SOLUTION > Stack Push

--- js/DataStructure/Stack/Exercises/Push.js
+++ js/DataStructure/Stack/Exercises/Push.js
@@ -1,10 +1,28 @@
-// ...
-
-class Stack {
-  // ...
-
-  // ...
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-export default Stack 
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+export default Stack;
