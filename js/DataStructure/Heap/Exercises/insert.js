34e0de71f6ea58f973af3a145ac4cf634ca53a95
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-20 12:44:34 +0330

    EXERCISE > Heap Insert (Code) (all)

--- js/DataStructure/Heap/Exercises/insert.js
+++ js/DataStructure/Heap/Exercises/insert.js
@@ -1,9 +1,7 @@
 export class Heap {
-  #heap = []
+  // ...
 
-  getHeap() {
-    return [...this.#heap]
-  }
+  // ...
 
   #leftChild(index) {
     return 2 * index + 1
@@ -13,13 +11,9 @@ export class Heap {
     return 2 * index + 2
   }
 
-  #parent(index) {
-    return Math.floor((index - 1) / 2)
-  }
+  // ...
 
-  #swap(index1, index2) {
-    [this.#heap[index1], this.#heap[index2]] = [this.#heap[index2], this.#heap[index1]]
-  }
+  // ...
 
   #sinkDown(index) {
     let maxIndex = index
@@ -45,14 +39,7 @@ export class Heap {
     }
   }
 
-  insert(value) {
-    this.#heap.push(value);
-    let current = this.#heap.length - 1;
-    while (current > 0 && this.#heap[current] > this.#heap[this.#parent(current)]) {
-      this.#swap(current, this.#parent(current));
-      current = this.#parent(current);
-    }
-  }
+  // ...
 
   remove() {
     if (this.#heap.length === 0) {
