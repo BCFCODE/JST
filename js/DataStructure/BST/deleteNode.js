d4b8396b4d530ae5d7c4d4f1d607d8ae86554e60
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-20 17:33:50 +0330

    SOLUTION > rBST Delete Code (3 of 3)

--- js/DataStructure/BST/deleteNode.js
+++ js/DataStructure/BST/deleteNode.js
@@ -102,8 +102,11 @@ class BST {
         currentNode = currentNode.right;
       } else if (currentNode.right === null) {
         currentNode = currentNode.left;
-      } 
-      // ...
+      } else {
+        const subTreeMin = this.minValue(currentNode.right);
+        currentNode.value = subTreeMin;
+        currentNode.right = this.#deleteNode(subTreeMin, currentNode.right);
+      }
     }
 
     return currentNode;
