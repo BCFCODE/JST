cfbd75be231adc9549c2d2eb8f7496f8c33f9b0e
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-20 17:40:16 +0330

    EXERCISE > rBST Insert

--- js/DataStructure/BST/rInsert.js
+++ js/DataStructure/BST/rInsert.js
@@ -1,85 +1,72 @@
 export class Node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class BST {
   constructor() {
-    this.root = null
+    this.root = null;
   }
 
   insert(value) {
     const newNode = new Node(value);
     if (this.root === null) {
-      this.root = newNode
-      return this
+      this.root = newNode;
+      return this;
     }
-    let temp = this.root
+    let temp = this.root;
     while (true) {
-      if (newNode === temp) return undefined
-      if (newNode.value === temp.value) return undefined
+      if (newNode === temp) return undefined;
+      if (newNode.value === temp.value) return undefined;
       if (newNode.value < temp.value) {
         if (temp.left === null) {
-          temp.left = newNode
-          return this
+          temp.left = newNode;
+          return this;
         }
-        temp = temp.left
+        temp = temp.left;
       } else {
         if (temp.right === null) {
-          temp.right = newNode
-          return this
+          temp.right = newNode;
+          return this;
         }
-        temp = temp.right
+        temp = temp.right;
       }
     }
   }
 
   contains(value) {
-    if (this.root === null) return false
-    let temp = this.root
+    if (this.root === null) return false;
+    let temp = this.root;
     while (temp) {
       if (value < temp.value) {
-        temp = temp.left
+        temp = temp.left;
       } else if (value > temp.value) {
-        temp = temp.right
+        temp = temp.right;
       } else {
-        return true
+        return true;
       }
     }
-    return false
+    return false;
   }
 
   rContains(value, currentNode = this.root) {
-    if (currentNode === null) return false
+    if (currentNode === null) return false;
 
-    if (value === currentNode.value) return true
+    if (value === currentNode.value) return true;
 
     if (value < currentNode.value) {
-      return this.rContains(value, currentNode.left)
+      return this.rContains(value, currentNode.left);
     } else {
-      return this.rContains(value, currentNode.right)
+      return this.rContains(value, currentNode.right);
     }
   }
 
-  #rInsert(value, currentNode = this.root) {
-    if (currentNode === null) return new Node(value)
+  // ...
 
-    if (value < currentNode.value) {
-      currentNode.left = this.#rInsert(value, currentNode.left)
-    } else if (value > currentNode.value) {
-      currentNode.right = this.#rInsert(value, currentNode.right)
-    }
-
-    return currentNode
-  }
-
-  rInsert(value) {
-    if (this.root === null) this.root = new Node(value)
-    this.#rInsert(value)
-  }
+  // ...
 }
 
-export default BST
+export default BST;
