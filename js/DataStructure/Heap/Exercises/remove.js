c9ae57344842ae00dc485a063f3f55072200ea0e
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-20 12:50:05 +0330

    EXERCISE > Heap Remove (all)

--- js/DataStructure/Heap/Exercises/remove.js
+++ js/DataStructure/Heap/Exercises/remove.js
@@ -1,49 +1,19 @@
 export class Heap {
-  #heap = []
+  // ...
 
-  getHeap() {
-    return [...this.#heap]
-  }
+  // ...
 
-  #leftChild(index) {
-    return 2 * index + 1
-  }
+  // ...
 
-  #rightChild(index) {
-    return 2 * index + 2
-  }
+  // ...
 
   #parent(index) {
     return Math.floor((index - 1) / 2)
   }
 
-  #swap(index1, index2) {
-    [this.#heap[index1], this.#heap[index2]] = [this.#heap[index2], this.#heap[index1]]
-  }
+  // ...
 
-  #sinkDown(index) {
-    let maxIndex = index
-    const size = this.#heap.length
-    while (true) {
-      const leftIndex = this.#leftChild(index)
-      const rightIndex = this.#rightChild(index)
-
-      if (leftIndex < size && this.#heap[leftIndex] > this.#heap[maxIndex]) {
-        maxIndex = leftIndex
-      }
-
-      if (rightIndex < size && this.#heap[rightIndex] > this.#heap[maxIndex]) {
-        maxIndex = rightIndex
-      }
-
-      if (maxIndex !== index) {
-        this.#swap(index, maxIndex)
-        index = maxIndex
-      } else {
-        return
-      }
-    }
-  }
+  // ...
 
   insert(value) {
     this.#heap.push(value);
@@ -54,19 +24,5 @@ export class Heap {
     }
   }
 
-  remove() {
-    if (this.#heap.length === 0) {
-      return null
-    }
-
-    if (this.#heap.length === 1) {
-      return this.#heap.pop()
-    }
-
-    const maxValue = this.#heap[0]
-    this.#heap[0] = this.#heap.pop()
-    this.#sinkDown(0)
-
-    return maxValue
-  }
+  // ...
 }
