f90c694ad6b0109a4e5d6fc73c0b58617d91bf96
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-25 12:03:37 +0330

    SOLUTION > LL Reverse

--- js/DataStructure/LL/Exercises/Reverse/EXERCISE-LL-Reverse.js
+++ js/DataStructure/LL/Exercises/Reverse/EXERCISE-LL-Reverse.js
@@ -1,7 +1,17 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
   printList() {
     let temp = this.head;
@@ -138,7 +148,20 @@ class LinkedList {
     return temp;
   }
 
-  // ...
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
 }
 
 function test() {
