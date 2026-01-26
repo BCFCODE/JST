fe173a81771b5f5bfd2d0fa34012a89de9847f17
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-01-26 12:51:44 +0330

    SOLUTION > DLL Get

--- js/DataStructure/DLL/Exercises/Get/EXERCISE-DLL-Get.js
+++ js/DataStructure/DLL/Exercises/Get/EXERCISE-DLL-Get.js
@@ -1,4 +1,10 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
 class DoublyLinkedList {
   constructor(value) {
@@ -100,7 +106,22 @@ class DoublyLinkedList {
     return temp;
   }
 
-  // ...
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    if (index < this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      temp = this.tail;
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
+    }
+    return temp;
+  }
+
 }
 
 
