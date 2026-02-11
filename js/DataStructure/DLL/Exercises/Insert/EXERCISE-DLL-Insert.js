7d3cf76fe22086e687adc3a8d9f4fce5ba543d7b
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-02-11 11:25:43 +0330

    SOLUTION > DLL Insert

--- js/DataStructure/DLL/Exercises/Insert/EXERCISE-DLL-Insert.js
+++ js/DataStructure/DLL/Exercises/Insert/EXERCISE-DLL-Insert.js
@@ -1,7 +1,18 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
 class DoublyLinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
 
   push(value) {
     const newNode = new Node(value);
@@ -86,8 +97,21 @@ class DoublyLinkedList {
     return false;
   }
 
-  // ...
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(value);
+    if (index === 0) return this.unshift(value);
 
+    const newNode = new Node(value);
+    const before = this.get(index - 1);
+    const after = before.next;
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
+    this.length++;
+    return true;
+  }
 }
 
-export default DoublyLinkedList
+export default DoublyLinkedList;
