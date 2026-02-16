767e6b9a98f647e838932e1dd2abc35ddab9b0a8
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-02-16 21:59:13 +0330

    SOLUTION > Remove Vertex

--- js/DataStructure/GR/Exercises/removeVertex.js
+++ js/DataStructure/GR/Exercises/removeVertex.js
@@ -1,35 +1,47 @@
 class Graph {
-  // ...
+  constructor() {
+    this.adjacencyList = {};
+  }
 
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = []
-      return true
+      this.adjacencyList[vertex] = [];
+      return true;
     }
-    return false
+    return false;
   }
 
   addEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1].push(vertex2)
-      this.adjacencyList[vertex2].push(vertex1)
-      return true
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
     }
-    return false
+    return false;
   }
 
   removeEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1] = this.adjacencyList[vertex1]
-        .filter(v => v !== vertex2)
-      this.adjacencyList[vertex2] = this.adjacencyList[vertex2]
-        .filter(v => v !== vertex1)
-      return true
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2,
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1,
+      );
+      return true;
     }
-    return false
+    return false;
   }
 
-  // ...
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+    while (this.adjacencyList[vertex].length) {
+      let temp = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, temp);
+    }
+    delete this.adjacencyList[vertex];
+    return this;
+  }
 }
 
-export default Graph
+export default Graph;
