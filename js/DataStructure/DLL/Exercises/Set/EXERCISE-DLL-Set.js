09d56fa0c8b9329cca3c93a5590df3b8b8eda0df
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-02-24 12:16:07 +0330

    SOLUTION > DLL Set

--- js/DataStructure/DLL/Exercises/Set/EXERCISE-DLL-Set.js
+++ js/DataStructure/DLL/Exercises/Set/EXERCISE-DLL-Set.js
@@ -1,7 +1,18 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
 class DoublyLinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
 
   printList() {
     let temp = this.head;
@@ -111,16 +122,21 @@ class DoublyLinkedList {
     return temp;
   }
 
-  // ...
-
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
 }
 
-
 function test() {
-  let myDLL = new DoublyLinkedList(0)
-  myDLL.push(1)
-  myDLL.push(2)
-  myDLL.push(3)
+  let myDLL = new DoublyLinkedList(0);
+  myDLL.push(1);
+  myDLL.push(2);
+  myDLL.push(3);
 
   console.log("DLL before set():");
   myDLL.printList();
@@ -131,9 +147,7 @@ function test() {
   myDLL.printList();
 }
 
-
 test();
-
 
 /*
     EXPECTED OUTPUT:
@@ -152,5 +166,4 @@ test();
 
 */
 
-
-export default DoublyLinkedList
+export default DoublyLinkedList;
