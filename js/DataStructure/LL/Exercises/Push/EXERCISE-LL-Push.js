fd01138309b10ef7f81f0e998ef2d97e67e0a156
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-03-11 14:13:19 +0330

    SOLUTION > LL Push

--- js/DataStructure/LL/Exercises/Push/EXERCISE-LL-Push.js
+++ js/DataStructure/LL/Exercises/Push/EXERCISE-LL-Push.js
@@ -1,7 +1,17 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
   printList() {
     let temp = this.head;
@@ -55,7 +65,18 @@ class LinkedList {
     return temp;
   }
 
-  // ...
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 function test() {
