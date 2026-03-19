fb1a690f14296a4b5ff08d8b72fc518cf305a180
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-03-19 10:44:10 +0330

    SOLUTION > DLL Pop

--- js/DataStructure/DLL/Exercises/Pop/EXERCISE-DLL-Pop.js
+++ js/DataStructure/DLL/Exercises/Pop/EXERCISE-DLL-Pop.js
@@ -1,7 +1,18 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
 
 class DoublyLinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
 
   printList() {
     let temp = this.head;
@@ -51,7 +62,20 @@ class DoublyLinkedList {
     return this;
   }
 
-  // ...
+  pop() {
+    if (this.length === 0) return undefined;
+    let temp = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
 
 function test() {
