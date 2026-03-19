6f2ae220b861144d6d742304a6f7b201e7e49819
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-03-19 10:13:38 +0330

    EXERCISE > LL Pop

--- js/DataStructure/LL/Exercises/Pop/EXERCISE-LL-Pop.js
+++ js/DataStructure/LL/Exercises/Pop/EXERCISE-LL-Pop.js
@@ -1,17 +1,7 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  } 
-}
+// ...
 
 class LinkedList {
-  constructor(value) {
-    const newNode = new Node(value);
-    this.head = newNode;
-    this.tail = this.head;
-    this.length = 1;
-  }
+  // ...
 
   printList() {
     let temp = this.head;
@@ -60,23 +50,7 @@ class LinkedList {
     return this;
   }
 
-  pop() {
-    if (this.length === 0) return undefined;
-    let temp = this.head;
-    let pre = this.head;
-    while (temp.next) {
-      pre = temp;
-      temp = temp.next;
-    }
-    this.tail = pre;
-    this.tail.next = null;
-    this.length--;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return temp;
-  }
+  // ...
 }
 
 function test() {
