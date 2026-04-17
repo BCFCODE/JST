ebbed8a2da7525e70af30936c9f43e5d05c54b09
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-17 14:35:30 +0330

    valid version of codes added ready to use in tests for validation

--- tests/DSA/valid/DLL.js
+++ tests/DSA/valid/DLL.js
@@ -2,10 +2,11 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     const newNode = new Node(value);
     this.head = newNode;
@@ -20,6 +21,7 @@ class LinkedList {
       this.tail = newNode;
     } else {
       this.tail.next = newNode;
+      newNode.prev = this.tail;
       this.tail = newNode;
     }
     this.length++;
@@ -27,30 +29,28 @@ class LinkedList {
   }
 
   pop() {
-    if (!this.head) return undefined;
-    let temp = this.head;
-    let pre = this.head;
-    while (temp.next) {
-      pre = temp;
-      temp = temp.next;
-    }
-    this.tail = pre;
-    this.tail.next = null;
-    this.length--;
-    if (this.length === 0) {
+    if (this.length === 0) return undefined;
+    const temp = this.tail;
+    if (this.length === 1) {
       this.head = null;
       this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
     }
+    this.length--;
     return temp;
   }
 
   unshift(value) {
     const newNode = new Node(value);
-    if (!this.head) {
+    if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
     } else {
       newNode.next = this.head;
+      this.head.prev = newNode;
       this.head = newNode;
     }
     this.length++;
@@ -58,24 +58,32 @@ class LinkedList {
   }
 
   shift() {
-    if (!this.head) return undefined;
-    const temp = this.head;
-    this.head = this.head.next;
-    temp.next = null;
-    this.length--;
-    if (this.length === 0) {
+    if (this.length === 0) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
       this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
     }
+    this.length--;
     return temp;
   }
 
   get(index) {
-    if (index < 0 || index >= this.length) {
-      return undefined;
-    }
+    if (index < 0 || index >= this.length) return undefined;
     let temp = this.head;
-    for (let i = 0; i < index; i++) {
-      temp = temp.next;
+    if (index < this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      temp = this.tail;
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
     }
     return temp;
   }
@@ -93,49 +101,29 @@ class LinkedList {
     if (index === 0) return this.unshift(value);
     if (index === this.length) return this.push(value);
     if (index < 0 || index > this.length) return false;
-
     const newNode = new Node(value);
-    const temp = this.get(index - 1);
-
-    newNode.next = temp.next;
-    temp.next = newNode;
+    const before = this.get(index - 1);
+    const after = before.next;
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
     this.length++;
     return true;
   }
 
   remove(index) {
-    if (index < 0 || index >= this.length) return undefined;
     if (index === 0) return this.shift();
     if (index === this.length - 1) return this.pop();
-
-    const before = this.get(index - 1);
-    const temp = before.next;
-
-    before.next = temp.next;
+    if (index < 0 || index >= this.length) return undefined;
+    const temp = this.get(index);
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
     temp.next = null;
-
+    temp.prev = null;
     this.length--;
-
     return temp;
-  }
-
-  reverse() {
-    let temp = this.head;
-    this.head = this.tail;
-    this.tail = temp;
-
-    let next = temp.next;
-    let prev = null;
-
-    for (let i = 0; i < this.length; i++) {
-      next = temp.next;
-      temp.next = prev;
-      prev = temp;
-      temp = next;
-    }
-
-    return this;
   }
 }
 
-export { LinkedList, Node };
+export { DoublyLinkedList, Node };
