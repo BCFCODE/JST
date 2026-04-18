93f27cf8597461de6d2bc91f3e82e3b873d987b5
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-18 13:11:59 +0330

    Shift tests implemented

--- js/tests/DSA/LL.js
+++ js/tests/DSA/LL.js
@@ -286,4 +286,99 @@ const Unshift = () => {
   });
 };
 
-export default [Push, Pop, Unshift];
+const Shift = () => {
+  describe("Before shift()\n\tlet myLinkedList = new LinkedList(2);\n\tmyLinkedList.push(1);", () => {
+    let correct = new LinkedList(2);
+    correct.push(1);
+
+    let myLinkedList = new LinkedList(2);
+    myLinkedList.push(1);
+
+    const tests = new Tests({
+      name: "myLinkedList",
+      my: myLinkedList,
+      correct,
+    });
+    const paths = {
+      toBeNull: ["head.next.next", "tail.next"],
+      toBe: ["head.value", "tail.value", "length"],
+      toEqual: ["head", "head.next", "tail"],
+    };
+    tests.toBeNull(paths.toBeNull);
+    tests.toBe(paths.toBe);
+    tests.toEqual(paths.toEqual);
+    tests.checkLLInternalStructure();
+    tests.run();
+  });
+
+  describe("After first shift()\n\tlet myLinkedList = new LinkedList(2);\n\tmyLinkedList.push(1);\n\tconst shiftReturnValue = myLinkedList.shift();", () => {
+    let correct = new LinkedList(2);
+    correct.push(1);
+    const correctShiftReturnValue = correct.shift();
+
+    let myLinkedList = new LinkedList(2);
+    myLinkedList.push(1);
+    const shiftReturnValue = myLinkedList.shift();
+
+    const tests = new Tests({
+      name: "myLinkedList",
+      my: myLinkedList,
+      correct,
+    });
+    const paths = {
+      toBeNull: ["head.next", "tail.next"],
+      toBe: ["head.value", "tail.value", "length"],
+      toEqual: ["head", "tail"],
+    };
+    tests.toBeNull(paths.toBeNull);
+    tests.toBe(paths.toBe);
+    tests.toEqual(paths.toEqual);
+    tests.checkLLInternalStructure();
+    tests.manual = [
+      [
+        "\n\tshiftReturnValue",
+        shiftReturnValue,
+        "toEqual",
+        correctShiftReturnValue,
+      ],
+    ];
+    tests.run();
+  });
+
+  describe("After second shift()\n\tlet myLinkedList = new LinkedList(2);\n\tmyLinkedList.push(1);\n\tmyLinkedList.shift();\n\tconst shiftReturnValue = myLinkedList.shift();", () => {
+    let correct = new LinkedList(2);
+    correct.push(1);
+    correct.shift();
+    correct.shift();
+    const correctShiftReturnValue = correct.shift();
+
+    let myLinkedList = new LinkedList(2);
+    myLinkedList.push(1);
+    myLinkedList.shift();
+    myLinkedList.shift();
+    const shiftReturnValue = myLinkedList.shift();
+
+    const tests = new Tests({
+      name: "myLinkedList",
+      my: myLinkedList,
+      correct,
+    });
+    const paths = {
+      toBeNull: ["head", "tail"],
+      toBe: ["length"],
+    };
+    tests.toBeNull(paths.toBeNull);
+    tests.toBe(paths.toBe);
+    tests.manual = [
+      [
+        "\n\tshiftReturnValue",
+        shiftReturnValue,
+        "toEqual",
+        correctShiftReturnValue,
+      ],
+    ];
+    tests.run();
+  });
+};
+
+export default [Push, Pop, Unshift, Shift];
