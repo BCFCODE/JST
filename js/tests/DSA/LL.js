8e4daff6a42126ed6a3ccf1c60c60a9b6c67ac5a
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-19 19:27:42 +0330

    Set tests implemented

--- js/tests/DSA/LL.js
+++ js/tests/DSA/LL.js
@@ -4,6 +4,8 @@ import { CorrectLinkedList } from "./correct/LL";
 import { Tests } from "../../utils";
 import Random from "../../utils/Random";
 
+const random = new Random();
+
 const Push = () => {
   describe("\n\tBefore push(value)\n\tlet myLinkedList = new LinkedList();\n\tmyLinkedList.pop(); (this.length = 0)", () => {
     const correct = new CorrectLinkedList();
@@ -383,17 +385,16 @@ const Shift = () => {
 };
 
 const Get = () => {
-  const randomIndex = new Random();
-  randomIndex.rangeLimit = 20;
+  random.rangeLimit = 20;
 
   const [index1, index2] = [0, 3];
 
-  const validIndex = randomIndex.between(index1, index2);
-  const invalidIndex = randomIndex.notBetween(index1, index2);
+  const validIndex = random.between(index1, index2);
+  const invalidIndex = random.notBetween(index1, index2);
 
   let myLinkedList = new LinkedList("Value Zero");
   myLinkedList.push("Value One");
-  myLinkedList.push("Value Two");  
+  myLinkedList.push("Value Two");
   myLinkedList.push("Value Three");
   const returnValueByRandomValidIndex = myLinkedList.get(validIndex);
   const returnValueByRandomInvalidIndex = myLinkedList.get(invalidIndex);
@@ -419,7 +420,7 @@ const Get = () => {
     tests.toBeNull(paths.toBeNull);
     tests.toBe(paths.toBe);
     tests.toEqual(paths.toEqual);
-    tests.checkLLInternalStructure(); 
+    tests.checkLLInternalStructure();
     tests.manual = [
       [
         "\n\treturnValueByRandomValidIndex",
@@ -437,5 +438,140 @@ const Get = () => {
     tests.run();
   });
 };
- 
-export default [Push, Pop, Unshift, Shift, Get];
+
+const Set = () => {
+  const length = 4;
+  const [val1, val2, val3, val4] = Array.from({ length }, (_, k) =>
+    random.greaterThan(k),
+  );
+
+  const randomValidIndex = random.between(0, length - 1);
+  const randomInvalidIndex = random.notBetween(0, length - 1);
+  const newValue = "New Value";
+
+  describe(`Before set any index\n\tlet myLinkedList = new LinkedList(${val1});\n\tmyLinkedList.push(${val2});\n\tmyLinkedList.push(${val3});\n\tmyLinkedList.push(${val4});`, () => {
+    let myLinkedList = new LinkedList(val1);
+    myLinkedList.push(val2);
+    myLinkedList.push(val3);
+    myLinkedList.push(val4);
+
+    let correct = new LinkedList(val1);
+    correct.push(val2);
+    correct.push(val3);
+    correct.push(val4);
+
+    const tests = new Tests({
+      name: "myLinkedList",
+      my: myLinkedList,
+      correct,
+    });
+    const paths = {
+      toBeNull: ["head.next.next.next.next", "tail.next"],
+      toBe: [
+        "head.value",
+        "head.next.value",
+        "head.next.next.value",
+        "head.next.next.next.value",
+        "tail.value",
+        "length",
+      ],
+      toEqual: ["head", "head.next", "tail"],
+    };
+    tests.toBeNull(paths.toBeNull);
+    tests.toBe(paths.toBe);
+    tests.toEqual(paths.toEqual);
+    tests.checkLLInternalStructure();
+    tests.run();
+  });
+
+  describe(`After set valid index\n\tlet myLinkedList = new LinkedList(${val1});\n\tmyLinkedList.push(${val2});\n\tmyLinkedList.push(${val3});\n\tmyLinkedList.push(${val4});\n\tmyLinkedList.set(${randomValidIndex}, "${newValue}")`, () => {
+    let myLinkedList = new LinkedList(val1);
+    myLinkedList.push(val2);
+    myLinkedList.push(val3);
+    myLinkedList.push(val4);
+    const setReturnValue = myLinkedList.set(randomValidIndex, newValue);
+
+    let correct = new LinkedList(val1);
+    correct.push(val2);
+    correct.push(val3);
+    correct.push(val4);
+    const correctSetReturnValue = correct.set(randomValidIndex, newValue);
+
+    const tests = new Tests({
+      name: "myLinkedList",
+      my: myLinkedList,
+      correct,
+    });
+    const paths = {
+      toBeNull: ["head.next.next.next.next", "tail.next"],
+      toBe: [
+        "head.value",
+        "head.next.value",
+        "head.next.next.value",
+        "head.next.next.next.value",
+        "tail.value",
+        "length",
+      ],
+      toEqual: ["head", "head.next", "tail"],
+    };
+    tests.toBeNull(paths.toBeNull);
+    tests.toBe(paths.toBe);
+    tests.toEqual(paths.toEqual);
+    tests.manual = [
+      [
+        `\n\tconst setReturnValue = myLinkedList.set(${randomValidIndex}, "${newValue}");\n\tsetReturnValue`,
+        setReturnValue,
+        "toBe",
+        correctSetReturnValue,
+      ],
+    ];
+    tests.checkLLInternalStructure();
+    tests.run();
+  });
+
+  describe(`After set INVALID index\n\tlet myLinkedList = new LinkedList(${val1});\n\tmyLinkedList.push(${val2});\n\tmyLinkedList.push(${val3});\n\tmyLinkedList.push(${val4});\n\tmyLinkedList.set(${randomInvalidIndex}, "${newValue}")`, () => {
+    let myLinkedList = new LinkedList(val1);
+    myLinkedList.push(val2);
+    myLinkedList.push(val3);
+    myLinkedList.push(val4);
+    const setReturnValue = myLinkedList.set(randomInvalidIndex, newValue);
+
+    let correct = new LinkedList(val1);
+    correct.push(val2);
+    correct.push(val3);
+    correct.push(val4);
+    const correctSetReturnValue = correct.set(randomInvalidIndex, newValue);
+
+    const tests = new Tests({
+      name: "myLinkedList",
+      my: myLinkedList,
+      correct,
+    });
+    const paths = {
+      toBeNull: ["head.next.next.next.next", "tail.next"],
+      toBe: [
+        "head.value",
+        "head.next.value",
+        "head.next.next.value",
+        "head.next.next.next.value",
+        "tail.value",
+        "length",
+      ],
+      toEqual: ["head", "head.next", "tail"],
+    };
+    tests.toBeNull(paths.toBeNull);
+    tests.toBe(paths.toBe);
+    tests.toEqual(paths.toEqual);
+    tests.manual = [
+      [
+        `\n\tconst setReturnValue = myLinkedList.set(${randomInvalidIndex}, "${newValue}");\n\tsetReturnValue`,
+        setReturnValue,
+        "toBe",
+        correctSetReturnValue,
+      ],
+    ];
+    tests.checkLLInternalStructure();
+    tests.run();
+  });
+};
+export default [Push, Pop, Unshift, Shift, Get, Set];
--- js/utils/Random.js
+++ js/utils/Random.js
@@ -29,13 +29,13 @@ class Random {
 
     const arr = [this.lessThan(n1), this.greaterThan(n2)];
     const randomArrayIndex = this.#randomArrayIndex(arr.length);
-
+ 
     const chosenRandomlyBetweenLessOrGreaterThan = arr[randomArrayIndex];
     return chosenRandomlyBetweenLessOrGreaterThan;
   };
-
+  
   set rangeLimit(rangeLimit) {
-    if (rangeLimit <= 0)
+    if (rangeLimit <= 0) 
       throw new Error(`rangeLimit: ${rangeLimit}, must be greater than 0`);
     this.#rangeLimit = rangeLimit;
   }
