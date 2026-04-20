b69e13a546ab2efae23d00eba86bed7f70a0c7d6
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-20 22:24:01 +0330

    SOLUTION > LL Pop

--- js/DSA/LL.js
+++ js/DSA/LL.js
@@ -1,7 +1,17 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
   push(value) {
     const newNode = new Node(value);
@@ -16,7 +26,23 @@ class LinkedList {
     return this;
   }
 
-  // ...
+  pop() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
 
   unshift(value) {
     const newNode = new Node(value);
