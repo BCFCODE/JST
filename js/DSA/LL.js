c40304b8093706be3fc48f2e407c0d881c4608ab
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-20 22:27:46 +0330

    EXERCISE > LL Unshift

--- js/DSA/LL.js
+++ js/DSA/LL.js
@@ -1,17 +1,7 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// ...
 
 class LinkedList {
-  constructor(value) {
-    const newNode = new Node(value);
-    this.head = newNode;
-    this.tail = this.head;
-    this.length = 1;
-  }
+  // ...
 
   push(value) {
     const newNode = new Node(value);
@@ -44,18 +34,7 @@ class LinkedList {
     return temp;
   }
 
-  unshift(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.length++;
-    return this;
-  }
+  // ...
 
   shift() {
     if (!this.head) return undefined;
