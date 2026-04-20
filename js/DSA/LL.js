37395a9f3cc67878e3b362369fd382f7e949394a
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-20 22:28:35 +0330

    SOLUTION > LL Unshift

--- js/DSA/LL.js
+++ js/DSA/LL.js
@@ -1,7 +1,17 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
   push(value) {
     const newNode = new Node(value);
@@ -34,7 +44,18 @@ class LinkedList {
     return temp;
   }
 
-  // ...
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 
   shift() {
     if (!this.head) return undefined;
