7e3acaa4027bb72e33d103620cc4bec0e8c1b785
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-20 22:39:56 +0330

    SOLUTION > LL Insert

--- js/DSA/LL.js
+++ js/DSA/LL.js
@@ -1,7 +1,17 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
   push(value) {
     const newNode = new Node(value);
@@ -79,7 +89,19 @@ class LinkedList {
     return false;
   }
 
-  // ...
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
 
   remove(index) {
     if (index < 0 || index >= this.length) return undefined;
