5b34ccacbbecdacec5d3238f95023e4cc2ddeea0
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-20 22:37:02 +0330

    EXERCISE > LL Set

--- js/DSA/LL.js
+++ js/DSA/LL.js
@@ -1,17 +1,7 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// ...
 
 class LinkedList {
-  constructor(value) {
-    const newNode = new Node(value);
-    this.head = newNode;
-    this.tail = this.head;
-    this.length = 1;
-  }
+  // ...
 
   push(value) {
     const newNode = new Node(value);
@@ -80,14 +70,7 @@ class LinkedList {
     return temp;
   }
 
-  set(index, value) {
-    let temp = this.get(index);
-    if (temp) {
-      temp.value = value;
-      return true;
-    }
-    return false;
-  }
+  // ...
 
   insert(index, value) {
     if (index === 0) return this.unshift(value);
