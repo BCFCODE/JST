363fe32f1995b2b2c112ef510932a0b99cfb865e
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-26 19:33:35 +0330

    Replace > EXERCISE (correct array without commas)

--- js/tests/TV-016/replace.test.js
+++ js/tests/TV-016/replace.test.js
@@ -5,10 +5,10 @@ describe(`Replace`, () => {
   it(`const names = [${names.reduce((str, name, i, arr) => str + `${JSON.stringify(name)}${i < arr.length - 1 ? ", " : ""}`, "")}];\n\tchangeNameAndSurname(names) > should create a new array that shows the names with the first name and then the surname.`, () => {
     const result = changeNameAndSurname(names);
     const correct = [
-      "James, Smith",
-      "Alyssa, Peterson",
-      "Lynette, Johnson",
-      "Tony, Lopez",
+      "James Smith",
+      "Alyssa Peterson",
+      "Lynette Johnson",
+      "Tony Lopez",
     ];
     expect(result).toEqual(correct);
   });
