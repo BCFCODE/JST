0b57b3bf2c12d8a9df01b408035ebe5a18290214
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-04-26 19:11:56 +0330

    Replace > EXERCISE

---
+++ js/TV-016/replace.js
@@ -0,0 +1,14 @@
+/*
+Create a new array that shows the names with the first name and then the surname.
+*/
+
+export const names = [
+  "Smith, James",
+  "Peterson, Alyssa",
+  "Johnson, Lynette",
+  "Lopez, Tony",
+];
+
+export function changeNameAndSurname(names) {
+
+}
