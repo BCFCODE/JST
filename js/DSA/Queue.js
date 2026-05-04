b022707a12b7ef784e070fe7076bfa57080d2ec7
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-05-04 13:03:37 +0330

    vitest updated to latest

--- js/DSA/Queue.js
+++ js/DSA/Queue.js
@@ -9,7 +9,7 @@ class Queue {
   constructor(value) {
     const newNode = new Node(value);
     this.first = newNode;
-    this.last = newNode;
+    this.last = newNode; 
     this.length = 1;
   }
 
