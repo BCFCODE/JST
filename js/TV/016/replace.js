48653c5d5f57b2999720e5449c13465b87133233
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-05-06 13:22:58 +0330

    Now one source of truth for TV dir

---
+++ js/TV/016/replace.js
@@ -0,0 +1,14 @@
+/*
+Create a new array that shows the names with the first name and then the surname.
+*/
+
+export const names = [
+  "Smith, James",
+  "Peterson, Alyssa",
+  "Johnson, Lynette",
+  "Lopez, Tony",
+];
+
+export function changeNameAndSurname(names) {
+  return names.map((name) => name.replace(/(\w+), (\w+)/, "$2 $1"));
+}
