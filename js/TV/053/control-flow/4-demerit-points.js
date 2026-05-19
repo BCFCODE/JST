b730624523c11bcff3e7bd8f1c8e6b7de346e2de
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-05-19 21:22:55 +0330

    SOLUTION-1 >>> 13- Exercise 4- Demerit Points

--- js/TV/053/control-flow/4-demerit-points.js
+++ js/TV/053/control-flow/4-demerit-points.js
@@ -2,7 +2,10 @@ export const SPEED_LIMIT = 70;
 export const ONE_POINT = 5;
 
 function checkSpeed(speed) {
-  
+  if (speed < SPEED_LIMIT + ONE_POINT) return "Ok";
+  const numberOfPoints = Math.floor((speed - SPEED_LIMIT) / ONE_POINT);
+  if (numberOfPoints <= 12) return `Point: ${numberOfPoints}`;
+  else return "License suspended";
 }
 
 export default checkSpeed;
