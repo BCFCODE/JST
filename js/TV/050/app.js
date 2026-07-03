a0d5e305751eabee793a6a6878d5948bd7f66b0e
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-07-03 15:58:34 +0330

    DOMContentLoaded event added

---
+++ js/TV/050/app.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const nav = document.querySelector('nav')
+    nav.innerHTML = `
+        <h1>Hello I am nav!</h1>
+    `
+    console.log(nav)
+})
