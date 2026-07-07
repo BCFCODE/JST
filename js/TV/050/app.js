928600747e9e63b572cbfad3cbdd3c132c99b952
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-07-07 14:29:23 +0330

    initialize the project

--- js/TV/050/app.js
+++ js/TV/050/app.js
@@ -1,7 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const nav = document.querySelector('nav')
-    nav.innerHTML = `
+  const nav = document.querySelector("nav");
+  nav.innerHTML = `
         <h1>Hello I am nav!</h1>
-    `
-    console.log(nav)
-})
+    `;
+  console.log(nav);
+});
+
+const $ = () => document.querySelector.call(this, arguments);
+const $$ = () => document.querySelectorAll.call(this, arguments);
+HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
+HTMLElement.prototype.$ = (s) => this.querySelector(s);
+HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
