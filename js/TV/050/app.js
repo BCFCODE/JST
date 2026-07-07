f60571f76ca081d6f3bfe28c3d5956c93707d2ac
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-07-07 14:54:50 +0330

    Creating Initial Services

--- js/TV/050/app.js
+++ js/TV/050/app.js
@@ -1,14 +1,23 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const nav = document.querySelector("nav");
-  nav.innerHTML = `
-        <h1>Hello I am nav!</h1>
-    `;
-  console.log(nav);
-});
+import Store from "./services/Store.js";
+import API from "./services/API.js";
 
-const $ = () => document.querySelector.call(this, arguments);
-const $$ = () => document.querySelectorAll.call(this, arguments);
-HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
-HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
-HTMLElement.prototype.$ = (s) => this.querySelector(s);
-HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
+window.app = {};
+app.store = Store;
+
+window.addEventListener("DOMContentLoaded", () => {
+  /*   
+    const nav = document.querySelector("nav");
+    nav.innerHTML = `
+          <h1>Hello I am nav!</h1>
+      `;
+    console.log(nav); 
+  */
+});
+/* 
+  const $ = () => document.querySelector.call(this, arguments);
+  const $$ = () => document.querySelectorAll.call(this, arguments);
+  HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+  HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
+  HTMLElement.prototype.$ = (s) => this.querySelector(s);
+  HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
+ */
