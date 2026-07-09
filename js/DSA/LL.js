951c83e756a0e228506cf2969ac0ddd061234628
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-07-09 19:52:13 +0330

    SOLUTION > LL Reverse

--- js/DSA/LL.js
+++ js/DSA/LL.js
@@ -1,7 +1,17 @@
-// ...
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
-  // ...
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
   push(value) {
     const newNode = new Node(value);
@@ -109,7 +119,23 @@ class LinkedList {
     return temp;
   }
 
-  // ...
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+
+    let next = temp.next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+
+    return this;
+  }
 }
 
 export { LinkedList, Node };
