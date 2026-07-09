4508118d4d9dbdb3c1934955b0ffd326b8cdb09e
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-07-09 21:09:14 +0330

    Router

--- js/TV/050/app.js
+++ js/TV/050/app.js
@@ -1,12 +1,15 @@
 import Store from "./services/Store.js";
 import API from "./services/API.js";
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
+  app.router.init();
 });
 /* 
   const $ = () => document.querySelector.call(this, arguments);
