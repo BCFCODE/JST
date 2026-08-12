df1e2d40484969c9d885154553fb4467bd84e57a
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-08-12 18:15:54 +0330

    EXERCISE > HT Keys

--- js/DSA/HT.js
+++ js/DSA/HT.js
@@ -1,15 +1,7 @@
 class HashTable {
-  constructor(size = 7) {
-    this.dataMap = new Array(size);
-  }
+  // ...
 
-  _hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
-    }
-    return hash;
-  }
+  // ...
 
   set(key, value) {
     let index = this._hash(key);
@@ -32,17 +24,7 @@ class HashTable {
     return undefined;
   }
 
-  keys() {
-    let allKeys = [];
-    for (let i = 0; i < this.dataMap.length; i++) {
-      if (this.dataMap[i]) {
-        for (let j = 0; j < this.dataMap[i].length; j++) {
-          allKeys.push(this.dataMap[i][j][0]);
-        }
-      }
-    }
-    return allKeys;
-  }
+  // ...
 }
 
 export { HashTable };
